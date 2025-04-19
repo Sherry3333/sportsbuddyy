392b13771fa6140afcb435b0119625ea8f2c4717
hiskyyang <hiskyrisa@gmail.com>
2025-04-19 21:01:35 +1200

    HX732-28 Add sport and location api

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,6 +4,12 @@ const router = express.Router();
 import user from "./user.js"
 router.use('/user', user);  
 
+import sport from "./sport.js"
+router.use('/sport', sport);
+
+import location from "./location.js"
+router.use('/location', location);
+
 import team from "./team.js"
 router.use('/team', team);
 
