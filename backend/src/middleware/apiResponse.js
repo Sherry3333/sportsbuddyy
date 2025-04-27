ff544f845548173d21ed9efa50b18f2c361cde7e
hiskyyang <hiskyrisa@gmail.com>
2025-04-28 10:34:43 +1200

    Extend response api

--- backend/src/middleware/apiResponse.js
+++ backend/src/middleware/apiResponse.js
@@ -6,6 +6,16 @@ export default function apiResponse(req, res, next) {
     res.apiError = (message = "Error", code = 500) => {
       res.status(500).json({ code, message });
     };
+
+    //401 Unauthorized
+    res.apiUnauthorized = (message = "Unauthorized", code = 401) => {
+      res.status(401).json({ code, message });
+    };
+    
+    //403 Forbidden
+    res.apiForbidden = (message = "Forbidden", code = 403) => {
+      res.status(403).json({ code, message });
+    };
     
     next();
   }
